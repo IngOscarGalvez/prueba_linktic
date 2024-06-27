78281aa7abd2f1ac917d947a9dc105a06c5fea9d
Oscar Galvez <ogalvez@netmetrix.co>
2024-06-27 08:44:57 -0500

    Areeglo rutas

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('tasks', TaskController::class);
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::apiResource('tasks', TaskController::class);
+});
