e9120e22c6fe1a7487497b3828344959c8cad2a7
Oscar Galvez <ogalvez@netmetrix.co>
2024-06-27 08:56:33 -0500

    Arreglos de task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+
+    const STATUS_PENDING = 'pending';
+    const STATUS_IN_PROGRESS = 'in_progress';
+    const STATUS_COMPLETED = 'completed';
+
+    /**
+     * The possible statuses for a task.
+     *
+     * @var array
+     */
+    public static $statuses = [
+        self::STATUS_PENDING,
+        self::STATUS_IN_PROGRESS,
+        self::STATUS_COMPLETED,
+    ];
 }
+
